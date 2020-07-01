9533f3242de07d5faf9f3dd7258e999f642d7103
Amy Nguyen <amy.yay22@gmail.com>
2020-07-01 18:46:22 +0000

    Reformat TODO comment

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -22,7 +22,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns some example content. */
+// TODO: modify this file to handle comments data 
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   private ArrayList<String> messages;
