131d248b1110cafeb542277c00b82290c7eea549
Amy Nguyen <amy.yay22@gmail.com>
2020-07-13 19:39:18 +0000

    Add init() for NicknameServlet.

--- portfolio/src/main/java/com/google/sps/servlets/NicknameServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/NicknameServlet.java
@@ -22,7 +22,15 @@ public class NicknameServlet extends HttpServlet {
   private static final String USER_INFO = "userInfo";
 
   private static final String BOTTOM_OF_PAGE = "/index.html#connect-container";
+
+  private static UserService userService;
+  private static DatastoreService datastore;
   
+  @Override
+  public void init() {
+    this.userService = UserServiceFactory.getUserService();
+    this.datastore = DatastoreServiceFactory.getDatastoreService();
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -30,7 +38,7 @@ public class NicknameServlet extends HttpServlet {
     PrintWriter out = response.getWriter();
     out.println("<h1>Set Nickname</h1>");
 
-    UserService userService = UserServiceFactory.getUserService();
+    this.userService = UserServiceFactory.getUserService();
     if (!userService.isUserLoggedIn()) {
       String loginUrl = userService.createLoginURL("/nickname");
       out.println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
@@ -49,21 +57,21 @@ public class NicknameServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
-    if (!userService.isUserLoggedIn()) {
+    this.userService = UserServiceFactory.getUserService();
+    if (!this.userService.isUserLoggedIn()) {
       response.sendRedirect("/nickname");
       return;
     }
 
     String nickname = request.getParameter(NICKNAME);
-    String id = userService.getCurrentUser().getUserId();
+    String id = this.userService.getCurrentUser().getUserId();
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    this.datastore = DatastoreServiceFactory.getDatastoreService();
     Entity entity = new Entity(USER_INFO, id);
     entity.setProperty(ID, id);
     entity.setProperty(NICKNAME, nickname);
     // The put() function automatically inserts new data or updates existing data based on ID.
-    datastore.put(entity);
+    this.datastore.put(entity);
 
     response.sendRedirect(BOTTOM_OF_PAGE);
   }
@@ -71,11 +79,11 @@ public class NicknameServlet extends HttpServlet {
   /** Returns an Optional object containing the nickname of the user with {@code id},
    * or an empty Optional if the user has not set a nickname. */
   private Optional<String> getUserNickname(String id) {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    this.datastore = DatastoreServiceFactory.getDatastoreService();
     Query query =
         new Query(USER_INFO)
             .setFilter(new Query.FilterPredicate(ID, Query.FilterOperator.EQUAL, id));
-    PreparedQuery results = datastore.prepare(query);
+    PreparedQuery results = this.datastore.prepare(query);
     Optional<Entity> optionalEntity = Optional.ofNullable(results.asSingleEntity());
     
     return optionalEntity.map(entity->(String)entity.getProperty(NICKNAME));
