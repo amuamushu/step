d2b961fe860399512f305a35e19578b99e7287bc
Amy Nguyen <35519361+amuamushu@users.noreply.github.com>
2020-07-15 13:15:33 -0700

    Revert "Revert "Add image Url to JSON returned by /data.""

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -31,6 +31,20 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Optional;
 
 
 /** Servlet that writes and returns comments data. */
@@ -49,6 +63,7 @@ public class DataServlet extends HttpServlet {
   private static final String COMMENT_LENGTH = "length";
   private static final String COMMENT_EMAIL = "email";
   private static final String COMMENT_NICKNAME = "nickname";
+  private static final String COMMENT_IMAGE_URL = "image";
   
   private static final String ANONYMOUS_AUTHOR = "anonymous";
   
@@ -64,36 +79,18 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     int maxComments = Integer.parseInt(request.getParameter(COMMENT_AMOUNT));
-    String sort = request.getParameter(SORT);
-
-    Query query = new Query(COMMENT_ENTITY);
-    if (sort.equals(OLDEST_FIRST)) {
-      query.addSort(COMMENT_TIMESTAMP, SortDirection.ASCENDING);
-    } else if (sort.equals(NEWEST_FIRST)) {
-      query.addSort(COMMENT_TIMESTAMP, SortDirection.DESCENDING);
-    } else if (sort.equals(LONGEST_FIRST)) {
-      query.addSort(COMMENT_LENGTH, SortDirection.DESCENDING);
-    }
-
+    Query query = sortedQuery(request);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
+    // TODO: Create helper method for creating all comments.
     ArrayList<Comment> comments = new ArrayList<>();
-
     int commentCounter = 0;
     for (Entity comment : results.asIterable()) {
       if (commentCounter == maxComments) {
         break;
       }
-
-      long id = comment.getKey().getId();
-      String text = (String) comment.getProperty(COMMENT_TEXT);
-      long timestamp = (long) comment.getProperty(COMMENT_TIMESTAMP);
-      String name = (String) comment.getProperty(COMMENT_NAME);
-      String nickname = (String) comment.getProperty(COMMENT_NICKNAME);
-      String email = (String) comment.getProperty(COMMENT_EMAIL);
-
-      comments.add(Comment.create(id, text, timestamp, name, email, nickname));
+      comments.add(createComment(comment));
       commentCounter++;
     } 
     
@@ -103,8 +100,40 @@ public class DataServlet extends HttpServlet {
   }
 
   /**
-  * Converts {@code toConvert} into a JSON string using GSON.
-  */
+   * Creates a Comment instance using properties from {@code comment}.
+   */
+  private Comment createComment(Entity comment) {
+    long id = comment.getKey().getId();
+    String text = (String) comment.getProperty(COMMENT_TEXT);
+    long timestamp = (long) comment.getProperty(COMMENT_TIMESTAMP);
+    String name = (String) comment.getProperty(COMMENT_NAME);
+    String nickname = (String) comment.getProperty(COMMENT_NICKNAME);
+    String imageUrl = (String) comment.getProperty(COMMENT_IMAGE_URL);
+
+    return Comment.builder().setId(id).setText(text).setTimestamp(timestamp)
+              .setName(name).setNickname(nickname).setImageUrl(imageUrl).build();
+  }
+
+  /**
+   * Returns a query sorted based on input from {@code request}.
+   */
+  private Query sortedQuery(HttpServletRequest request) {
+    String sort = request.getParameter(SORT);
+    Query query = new Query(COMMENT_ENTITY);
+
+    if (sort.equals(OLDEST_FIRST)) {
+      query.addSort(COMMENT_TIMESTAMP, SortDirection.ASCENDING);
+    } else if (sort.equals(NEWEST_FIRST)) {
+      query.addSort(COMMENT_TIMESTAMP, SortDirection.DESCENDING);
+    } else if (sort.equals(LONGEST_FIRST)) {
+      query.addSort(COMMENT_LENGTH, SortDirection.DESCENDING);
+    }
+    return query;
+  }
+
+  /**
+   * Converts {@code toConvert} into a JSON string using GSON.
+   */
   private String convertToJsonUsingGson(List toConvert) {
     Gson gson = new Gson();
     String json = gson.toJson(toConvert);
@@ -113,14 +142,11 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
-    String email = userService.getCurrentUser().getEmail();
-    System.out.println(email);
-
     String text = request.getParameter(COMMENT_INPUT);
     long timestamp = System.currentTimeMillis();
     String name = (String) request.getParameter(COMMENT_NAME);
     String nickname = HomeServlet.getUserNickname();
+    String imageUrl = getUploadedFileUrl(request, COMMENT_IMAGE_URL).orElse("");
 
     if (name.isEmpty()) {
       name = ANONYMOUS_AUTHOR;
@@ -131,13 +157,54 @@ public class DataServlet extends HttpServlet {
     commentEntity.setProperty(COMMENT_TIMESTAMP, timestamp);
     commentEntity.setProperty(COMMENT_NAME, name);
     commentEntity.setProperty(COMMENT_LENGTH, text.length());
-    commentEntity.setProperty(COMMENT_EMAIL, email);
     commentEntity.setProperty(COMMENT_NICKNAME, nickname);
+    commentEntity.setProperty(COMMENT_IMAGE_URL, imageUrl);
     
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
 
     // Redirects to the bottom of the current page to see new comment added.
     response.sendRedirect(BOTTOM_OF_PAGE);
+  }
+
+  /** 
+   * Returns a URL that points to the uploaded file based on user input in 
+   * {@code formInputElementName}. If the user didn't upload a file, returns an empty Optional.
+   */
+  private Optional<String> getUploadedFileUrl(HttpServletRequest request,
+      String formInputElementName) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    Optional<List<BlobKey>> blobKeys = Optional.ofNullable(blobs.get("image"));
+    
+    // Cannot get a URL because the user submitted the form without selecting a file.
+    if (!blobKeys.isPresent() || blobKeys.get().isEmpty()) {
+      return Optional.empty();
+    }
+
+    // Gets the first index because the comment form only takes in one file input.
+    BlobKey blobKey = blobKeys.get().get(0);
+
+    // Cannot get a URL because the user submitted the form on the live server without 
+    // selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return Optional.empty();
+    }
+
+    // TODO: Check that the file uploaded has an image extension.
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+
+    // To support running in Google Cloud Shell with AppEngine's devserver, uses the relative
+    // path to the image, rather than the path returned by imagesService.
+    try {
+      URL url = new URL(imagesService.getServingUrl(options));
+      return Optional.of(url.getPath());
+    } catch (MalformedURLException e) {
+      return Optional.of(imagesService.getServingUrl(options));
+    }
   }
 }
