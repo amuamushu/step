6431a505b479d44bd13d7da7e3b36b1ae337b50c
Amy Nguyen <35519361+amuamushu@users.noreply.github.com>
2020-07-15 13:05:49 -0700

    Revert "Add image Url to JSON returned by /data."

--- portfolio/src/main/java/com/google/sps/servlets/HomeServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HomeServlet.java
@@ -66,10 +66,8 @@ public class HomeServlet extends HttpServlet {
     response.getWriter().println("<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>");
   }
 
-  /** 
-   * Returns an Optional object containing the nickname of the user with {@code id},
-   * or an empty Optional if the user has not set a nickname. 
-   */
+  /** Returns an Optional object containing the nickname of the user with {@code id},
+   * or an empty Optional if the user has not set a nickname. */
   private Optional<String> getUserNickname(String id) {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Query query =
