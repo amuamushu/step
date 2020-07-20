0b8255fa1cdecfded81de68d765d69ab7ad4ce68
Amy Nguyen <amy.yay22@gmail.com>
2020-07-20 20:28:49 +0000

    Add helper method for query().

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -22,22 +22,24 @@ import java.util.Set;
 
 /** Finds potential meeting times. */
 public final class FindMeetingQuery {
+  Collection<TimeRange> timesForEveryone; //= new ArrayList<>();
+  Collection<TimeRange> timesForConfirmed; //= new ArrayList<>();
 
   /**
    * Returns available TimeRanges for the {@code request} given {@code events}, a Collection of
    * all events so far. 
    */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    Collection<TimeRange> timesForEveryone = new ArrayList<>();
-    Collection<TimeRange> timesForConfirmed = new ArrayList<>();
-    Collection<String> attendees = request.getAttendees();
-    Collection<String> optionalAttendees = request.getOptionalAttendees();
+    //Collection<TimeRange> timesForEveryone = new ArrayList<>();
+    //Collection<TimeRange> timesForConfirmed = new ArrayList<>();
+    timesForEveryone = new ArrayList<>();
+    timesForConfirmed = new ArrayList<>();
 
     if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
       // If the event is longer than a day, no time slots are available to book a meeting.
       return timesForEveryone; 
     }
-    if (attendees.size() == 0 && optionalAttendees.size() == 0) {
+    if (request.getAttendees().size() == 0 && request.getOptionalAttendees().size() == 0) {
       // If there are no attendees, the entire day is available to book a meeting.
       timesForEveryone.add(TimeRange.WHOLE_DAY);
       return timesForEveryone;
@@ -48,41 +50,42 @@ public final class FindMeetingQuery {
       // Ignores this event's time range if the people attending this event are 
       // not attending the requested meeting. 
       Set<String> eventAttendees = new HashSet<String>(event.getAttendees());
-      eventAttendees.retainAll(attendees);
-      Set<String> optionalEventAttendeesForRequestedMeeting = new HashSet<String>(event.getAttendees());
-      optionalEventAttendeesForRequestedMeeting.retainAll(optionalAttendees);
+      eventAttendees.retainAll(request.getAttendees());
+      Set<String> optionalEventAttendeesForRequestedMeeting = 
+          new HashSet<String>(event.getAttendees());
+      optionalEventAttendeesForRequestedMeeting.retainAll(request.getOptionalAttendees());
       if (eventAttendees.isEmpty() && optionalEventAttendeesForRequestedMeeting.isEmpty()) {
         continue;
       }
 
       TimeRange time = event.getWhen();
+      // // Ignores this all-day event if only optional attendees are present.
+      // if (time.equals(TimeRange.WHOLE_DAY) && eventAttendees.isEmpty()) {
+      //   continue;
+      // }
 
-      // Ignores this all-day event if only optional attendees are present.
-      if (time.equals(TimeRange.WHOLE_DAY) && eventAttendees.isEmpty()) {
-        continue;
-      }
+      // // Handles overlapping events and the case where the requested meeting duration is longer
+      // // than the time gap.
+      // if ((time.start() <= currentEndTime) || (time.end() <= currentEndTime) ||
+      //     (request.getDuration() > (time.start() - currentEndTime))) {
+      //   // If only optional attendees are attending this event, then this time slot would work for 
+      //   // confirmed attendees.
+      //   if (eventAttendees.isEmpty()) {
+      //     int latestEndTime = (int) Math.max(time.start(), currentEndTime + request.getDuration());
+      //     TimeRange gapTimeRange = TimeRange.fromStartEnd(currentEndTime, latestEndTime, false);
+      //     timesForConfirmed.add(gapTimeRange);
+      //     currentEndTime = latestEndTime;
+      //   }
+      //   currentEndTime = Math.max(time.end(), currentEndTime);
+      //   continue;
+      // }
 
-      // Handles overlapping events and the case where the requested meeting duration is longer
-      // than the time gap.
-      if ((time.start() <= currentEndTime) || (time.end() <= currentEndTime) ||
-          (request.getDuration() > (time.start() - currentEndTime))) {
-        // If only optional attendees are attending this event, then this time slot would work for 
-        // confirmed attendees.
-        if (eventAttendees.isEmpty()) {
-          int latestEndTime = (int) Math.max(time.start(), currentEndTime + request.getDuration());
-          TimeRange gapTimeRange = TimeRange.fromStartEnd(currentEndTime, latestEndTime, false);
-          timesForConfirmed.add(gapTimeRange);
-          currentEndTime = latestEndTime;
-        }
-        currentEndTime = Math.max(time.end(), currentEndTime);
-        continue;
-      }
-
-      TimeRange gapTimeRange = TimeRange.fromStartEnd(currentEndTime, time.start(), false);
-      currentEndTime = time.end();
-      timesForEveryone.add(gapTimeRange);
+      // TimeRange gapTimeRange = TimeRange.fromStartEnd(currentEndTime, time.start(), false);
+      // currentEndTime = time.end();
+      // timesForEveryone.add(gapTimeRange);
+      currentEndTime = addTimeGapIfPossibleAndReturnLatestEndTime(request, eventAttendees, time, currentEndTime);
     }
-
+    
     // Adds a timerange for after the latest event ends.
     if (currentEndTime < TimeRange.END_OF_DAY) {
       TimeRange latestTimeRange = TimeRange.fromStartEnd(currentEndTime, 
@@ -90,9 +93,37 @@ public final class FindMeetingQuery {
       timesForEveryone.add(latestTimeRange);
     }
 
-    if (timesForEveryone.size() == 0 && attendees.size() != 0) {
+    if (timesForEveryone.size() == 0 && request.getAttendees().size() != 0) {
       return timesForConfirmed;
     }
     return timesForEveryone;
   }
+
+  public int addTimeGapIfPossibleAndReturnLatestEndTime(MeetingRequest request, Set<String> eventAttendees, TimeRange time, int currentEndTime) {
+    // Ignores this all-day event if only optional attendees are present.
+    if (time.equals(TimeRange.WHOLE_DAY) && eventAttendees.isEmpty()) {
+      return currentEndTime;
+    }
+
+    // Handles overlapping events and the case where the requested meeting duration is longer
+    // than the time gap.
+    if ((time.start() <= currentEndTime) || (time.end() <= currentEndTime) ||
+        (request.getDuration() > (time.start() - currentEndTime))) {
+      // If only optional attendees are attending this event, then this time slot would work for 
+      // confirmed attendees.
+      if (eventAttendees.isEmpty()) {
+        int latestEndTime = (int) Math.max(time.start(), currentEndTime + request.getDuration());
+        TimeRange gapTimeRange = TimeRange.fromStartEnd(currentEndTime, latestEndTime, false);
+        timesForConfirmed.add(gapTimeRange);
+        currentEndTime = latestEndTime;
+      }
+      currentEndTime = Math.max(time.end(), currentEndTime);
+      return currentEndTime;
+    }
+
+    TimeRange gapTimeRange = TimeRange.fromStartEnd(currentEndTime, time.start(), false);
+    currentEndTime = time.end();
+    timesForEveryone.add(gapTimeRange);
+    return currentEndTime;
+    }
 }
